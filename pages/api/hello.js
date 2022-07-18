f0871ca10e71977ab71a4d85191b4a757f65c99f
Amit Patkar <amitpatkar@gmail.com>
2022-07-18 08:23:42 -0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  console.log(req.json);
+  console.log("Amit Patkar");
   res.status(200).json({ name: 'Amit Patkar' })
 }
