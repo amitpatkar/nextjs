661256741ddea4c5223dbf281c5723768566543b
Amit Patkar <amitpatkar@gmail.com>
2022-07-18 09:55:23 -0700

    Update csp.js

--- pages/api/csp.js
+++ pages/api/csp.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  console.log(req);
-  res.status(200).json({ name: 'CSP Logging 1' })
+  console.log(req.body);
+  res.status(200).json({ name: 'CSP Logging 3' })
 }
